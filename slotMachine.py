ad0518527ed5c6c5175c3e203c9258b35578a292
Soumya Dubey <10a.soumyadubey@gmail.com>
2025-06-02 17:44:59 -0400

    Initial coding

---
+++ slotMachine.py
@@ -0,0 +1,75 @@
+import random
+
+MAX_LINES=3
+MAX_BET=100
+MIN_BET=1
+
+ROWS, COLS=3
+
+SYMBOL_COUNT={
+    "A":3,
+    "B":3,
+    "C":3,
+    "D":3
+}
+
+
+def deposit():
+    while True:
+        amount=input("What would you like to deposit? $ ")
+        if amount.isdigit():
+            amount=int(amount)
+            if amount>0:
+                break
+            else:
+                print("Amount must be greater than 0. ")
+        else:
+            print("Please enter a number. ")
+    return amount
+
+def get_no_of_lines():
+    while True:
+        lines=input("Enter the number of lines to bet on (1 - "+str(MAX_LINES)+") ? ")
+        if lines.isdigit():
+            lines=int(lines)
+            if 1 <= lines <=MAX_LINES:
+                break
+            else:
+                print("Enter valid number of lines. . ")
+        else:
+            print("Please enter a number. ")
+    return lines
+
+def get_bet():
+    while True:
+        amount=input("What would you like to bet? $ ")
+        if amount.isdigit():
+            amount=int(amount)
+            if MIN_BET<=amount<=MAX_BET:
+                break
+            else:
+                print(f"Amount must be between $ {MIN_BET} - $ {MAX_BET}. ")
+        else:
+            print("Please enter a number. ")
+    return amount
+
+
+def main():
+    balance = deposit()
+    lines=get_no_of_lines()
+    while True:
+        bet=get_bet()
+        total_bet=bet*lines
+
+        if total_bet>balance:
+            print(f"You do not have enough to bet that amount. Current balance is $ {balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: $ {bet*lines}")
+
+
+
+
+main()
+                
